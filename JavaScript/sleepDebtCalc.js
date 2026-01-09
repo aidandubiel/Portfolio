935a87d2ee745dd49f5ff5f8ed1d9f76272569ce
aidandubiel <78991149+aidandubiel@users.noreply.github.com>
2026-01-09 01:28:04 -0500

    Clean up getSleepHours function
    Removed unnecessary break statements in getSleepHours function.

--- JavaScript/sleepDebtCalc.js
+++ JavaScript/sleepDebtCalc.js
@@ -2,25 +2,18 @@ const getSleepHours = day => {
   switch (day){
     case 'monday':
       return 6;
-      break;
     case 'tuesday':
       return 6;
-      break;
     case 'wednesday':
       return 8;
-      break;
     case 'thursday':
       return 8;
-      break;
     case 'friday':
       return 8;
-      break;
     case 'saturday':
     return 10;
-    break;
     case 'sunday':
     return 6;
-    break;
   }
 }
 
